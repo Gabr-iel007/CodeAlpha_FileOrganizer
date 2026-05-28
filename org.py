85de67436c74911137a52e1780419938e13cf8e4
Gabr-iel <kayodeolawunmi117@gmail.com>
2026-05-28 15:45:44 +0100

    Made some comments

--- org.py
+++ org.py
@@ -1,13 +1,13 @@
 import os
 import shutil
 
-folder_path = input("Enter folder path: ").strip()
+folder_path = input("Enter folder path: ").strip() # include folder path you want to organize
 
 files = os.listdir(folder_path)
 
-print(files)
+print(files) #lists all files in the directory
 
-os.makedirs(folder_path + "\\Images", exist_ok=True)
+os.makedirs(folder_path + "\\Images", exist_ok=True)#creates folders
 os.makedirs(folder_path + "\\Videos", exist_ok=True)
 os.makedirs(folder_path + "\\Music", exist_ok=True)
 os.makedirs(folder_path + "\\Document", exist_ok=True)
@@ -15,11 +15,11 @@ os.makedirs(folder_path + "\\Document", exist_ok=True)
 for file in files:
     name, extension = os.path.splitext(file)
 
-    if extension in [".jpg", ".jpeg", ".png"]:
-        source = os.path.join(folder_path, file)
-        destination = os.path.join(folder_path, "Images")
-        shutil.move(source, destination)
-        print(file, "-> Images")
+    if extension in [".jpg", ".jpeg", ".png"]: #checks for extensions of each file
+        source = os.path.join(folder_path, file)# source of file
+        destination = os.path.join(folder_path, "Images")# destination of file
+        shutil.move(source, destination) #move file to new destination
+        print(file, "-> Images") # file type, image
 
     elif extension in [".mp4", ".mkv"]:
         source = os.path.join(folder_path, file)
